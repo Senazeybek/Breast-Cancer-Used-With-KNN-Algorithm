e23dca5565974ee3b66c45457975c44df8ac72f0
Sena Zeybek <83763313+flyinhor@users.noreply.github.com>
2024-05-27 14:57:25 +0300

    Update kanserverisi.py

--- kanserverisi.py
+++ kanserverisi.py
@@ -35,7 +35,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # KNN modelini oluşturma
-knn = KNeighborsClassifier(n_neighbors=2)
+knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
 
 # Test seti ile tahmin yapma
